dbd2c919608e08eb57ee9eb45db08d95885a4ca6
Yuanhua Huang <huangyh094@gmail.com>
2018-01-31 07:02:45 +0000

    update sim_read_count and add get_snv_theta
    Former-commit-id: 9e27e81b0f414fd247bf51c559f47dfe0befe106

--- R/base_utils.R
+++ R/base_utils.R
@@ -33,3 +33,30 @@ get_prob_label <- function(prob_assign){
   }
   assign_clone
 }
+
+
+#' Calculate the SNV specific allelic frequency (True postive rate) and error
+#' rate (False possitive rate)
+#' 
+#' @export
+get_snv_theta <- function(C, D, A, prob_mat, threshold=1){
+  ## preprocessing
+  N <- dim(A)[1] # number of variants 
+  M <- dim(A)[2] # number of cells
+  K <- dim(C)[2] # number of clones
+  
+  A[is.na(A)] <- 0  # number of alteration reads
+  D[is.na(D)] <- 0  # number of total reads
+  
+  snv_theta <- matrix(0, nrow = N, ncol = 2)
+  for (n in seq_len(N)){
+    a0 <- sum(A[n,] * prob_mat * (1-C[n,]))  #Alteration reads without variants
+    d0 <- sum(D[n,] * prob_mat * (1-C[n,]))  #Total reads without variants (C=0)
+    a1 <- sum(A[n,] * prob_mat * C[n,])  #Alteration reads with variants
+    d1 <- sum(D[n,] * prob_mat * C[n,])  #Total reads with variants (C=1)
+    
+    snv_theta[n,1] <- a0 / d0
+    snv_theta[n,2] <- a1 / d1
+  }
+  snv_theta
+}
