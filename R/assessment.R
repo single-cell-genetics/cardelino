a72429eb89c75895f6dabbcd44da8a06e5b4c06f
Yuanhua Huang <huangyh094@gmail.com>
2018-05-19 21:22:56 +0100

    update assessment functions
    Former-commit-id: 24e912a90883b9024a9e50abc508e75f5accd2df

--- R/assessment.R
+++ R/assessment.R
@@ -8,51 +8,64 @@
 #' @param prob_mat A matrix (M cells x K clones), estiamted probability of cell 
 #' to clone
 #' @param simu_mat A matrix (M cells x K clones), tru cell-clone assignment
-#' @param prob_gap_min A float value, the threshold for assignable cells
+#' @param threshold A float value, the threshold for assignable cells
+#' @param mode A string, the mothod for defining scores for filtering cells: 
+#' best and delta. best: highest probability of a cell to K clones, delta: the 
+#' difference between the best and second.
 #' @export
 #' 
-assign_score <- function(prob_mat, simu_mat, prob_gap_min=0.2){
+assign_score <- function(prob_mat, simu_mat, threshold=0.2, mode="delta"){
   assign_0 <- cardelino::get_prob_label(simu_mat)
   assign_1 <- cardelino::get_prob_label(prob_mat)
-  prob_gap <- cardelino::get_prob_gap(prob_mat)
-  idx <- prob_gap >= prob_gap_min
+  prob_val <- cardelino::get_prob_value(prob_mat, mode=mode)
+  idx <- prob_val >= threshold
   
-  #return: assignable cells, accuracy with assignable cells, overall accuracy
-  c(mean(idx), mean((assign_0 == assign_1)[idx]), mean(assign_0 == assign_1))
+  rt_list <- list("ass"=mean(idx), 
+                  "acc"=mean(assign_0 == assign_1),
+                  "acc_ass"=mean((assign_0 == assign_1)[idx]))
+  rt_list
 }
 
 #' Calculate assignability and accuracy along prob_gap
 #' 
 #' @param prob_mat A matrix (M cells x K clones), estiamted probability of cell 
 #' to clone
-#' @param simu_mat A matrix (M cells x K clones), tru cell-clone assignment
+#' @param simu_mat A matrix (M cells x K clones), true cell-clone assignment
+#' @param mode A string, the mothod for defining scores for filtering cells: 
+#' best, second and delta. Best: highest probability of a cell to K clones, 
+#' similarly for second. delta is the difference between the best and second.
+#' 
 #' @export
 #' 
-assign_curve <- function(prob_mat, simu_mat){
+assign_curve <- function(prob_mat, simu_mat, mode="delta"){
   assign_0 <- cardelino::get_prob_label(simu_mat)
   assign_1 <- cardelino::get_prob_label(prob_mat)
-  prob_gap <- cardelino::get_prob_gap(prob_mat)
+  prob_val <- cardelino::get_prob_value(prob_mat, mode=mode)
   
-  GAP <- sort(unique(prob_gap))
-  ACC <- rep(0, length(GAP))
-  ASS <- rep(0, length(GAP))
-  for(i in seq_len(length(GAP))){
-    idx <- prob_gap >= GAP[i] 
-    #if(GAP[i] == 0){ idx <- prob_gap > GAP[i] }
+  thresholds <- sort(unique(prob_val))
+  ACC <- rep(0, length(thresholds))
+  ASS <- rep(0, length(thresholds))
+  for(i in seq_len(length(thresholds))){
+    idx <- prob_val >= thresholds[i] 
+    #if(thresholds[i] == 0){ idx <- prob_val > thresholds[i] }
     ASS[i] <- mean(idx)
     ACC[i] <- mean((assign_0 == assign_1)[idx])
   }
-  GAP <- c(GAP, 1.0)
+  thresholds <- c(thresholds, 1.0)
   ACC <- c(ACC, 1.0)
   ASS <- c(ASS, 0.0)
   
-  AUC <- 0.0
-  for(i in seq_len(length(GAP)-1)){
-    AUC <- AUC + 0.5 * (ASS[i] - ASS[i+1]) * (ACC[i] + ACC[i+1])
+  AUC <- AUC_acc_ass <- 0.0
+  for(i in seq_len(length(thresholds)-1)){
+    AUC <- AUC + 0.5 * (thresholds[i] - thresholds[i+1]) * (ACC[i] + ACC[i+1])
+    AUC_acc_ass <- AUC_acc_ass + 0.5 * (ASS[i] - ASS[i+1]) * (ACC[i] + ACC[i+1])
   }
-  AUC <- AUC / (ASS[1] - ASS[length(GAP)])
+  AUC <- AUC / (thresholds[1] - thresholds[length(thresholds)])
+  AUC_acc_ass <- AUC_acc_ass / (ASS[1] - ASS[length(thresholds)])
   
-  rt_list <- list("GAP"=GAP, "ACC"=ACC, "ASS"=ASS, "AUC"=AUC)
+  
+  rt_list <- list("ACC"=ACC, "ASS"=ASS, "AUC"=AUC, "AUC_acc_ass"=AUC_acc_ass,
+                  "thresholds"=thresholds)
   rt_list
 }
 
@@ -65,13 +78,17 @@ assign_curve <- function(prob_mat, simu_mat){
 #' @export
 #' 
 assign_macro_ROC <- function(prob_mat, simu_mat){
-  thresholds <- seq(0, 1, 0.001)
+  thresholds <- seq(0, 0.999, 0.001)
+  ACC <- rep(0, length(thresholds))
+  ASS <- rep(0, length(thresholds))
   FPR <- rep(0, length(thresholds))
   TPR <- rep(0, length(thresholds))
   for(i in seq_len(length(thresholds))){
-    pred_mat <- prob_mat >= thresholds[i]
-    FPR[i] <- sum((simu_mat == 0) & (pred_mat == 1)) / sum(simu_mat == 0)
-    TPR[i] <- sum((simu_mat == 1) & (pred_mat == 1)) / sum(simu_mat == 1)
+    idx <- prob_mat >= thresholds[i]
+    ASS[i] <- mean(idx) # not very meaningful
+    ACC[i] <- mean(simu_mat[idx])
+    FPR[i] <- sum(simu_mat[idx] == 0) / sum(simu_mat == 0)
+    TPR[i] <- sum(simu_mat[idx] == 1) / sum(simu_mat == 1)
   }
   
   AUC <- 0.0
@@ -79,7 +96,8 @@ assign_macro_ROC <- function(prob_mat, simu_mat){
     AUC <- AUC + 0.5 * (FPR[i] - FPR[i+1]) * (TPR[i] + TPR[i+1])
   }
   
-  rt_list <- list("thresholds"=thresholds, "FPR"=FPR, "TPR"=TPR, "AUC"=AUC)
+  rt_list <- list("FPR"=FPR, "TPR"=TPR, "AUC"=AUC,
+                  "thresholds"=thresholds, "ACC"=ACC, "ASS"=ASS)
   rt_list
 }
 
