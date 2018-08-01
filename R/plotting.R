e82f5abb2039c73d21cd03d40d40308d2402ef20
davismcc <davismcc@gmail.com>
2018-08-01 12:09:08 +0100

    Updating donor_id() and fixing imports.

--- R/plotting.R
+++ R/plotting.R
@@ -232,7 +232,7 @@ vc_heatmap <- function(mat, prob, Config, show_legend=FALSE){
 #'
 #' @return a ggtree object
 #'
-#' @import ggtree
+#' @importFrom ggtree ggtree geom_tiplab
 #' @import ggplot2
 #'
 #' @author Davis McCarthy and Yuanhua Huang
@@ -240,7 +240,7 @@ vc_heatmap <- function(mat, prob, Config, show_legend=FALSE){
 #' @export
 #'
 #' @references
-#' This function makes use of the \code{\link{ggtree}} packages:
+#' This function makes use of the \code{\link{ggtree}} package:
 #'
 #' Guangchuang Yu, David Smith, Huachen Zhu, Yi Guan, Tommy Tsan-Yuk Lam.
 #' ggtree: an R package for visualization and annotation of phylogenetic trees with their covariates and other associated data. Methods in Ecology and Evolution 2017, 8(1):28-36, doi:10.1111/2041-210X.12628
