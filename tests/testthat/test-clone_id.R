fcc36b30bc1e130e1892af2eb059fa561224952c
davismcc <davismcc@gmail.com>
2018-08-17 13:25:36 +0100

    Adding first tests.

---
+++ tests/testthat/test-clone_id.R
@@ -0,0 +1,28 @@
+# Tests for clone_id methods
+# library(cardelino); library(testthat); source("test-clone_id.R")
+
+context("test clone ID")
+
+data(example_donor)
+
+test_that("default inference works as expected", {
+    assignments <- clone_id(A, D, C = tree$Z)
+    expect_is(assignments, "list")
+})
+
+test_that("binomial EM inference works as expected", {
+    assignments_EM <- clone_id(A, D, C = tree$Z, inference = "EM")
+    expect_is(assignments_EM, "list")
+})
+
+test_that("Bernoulli Gibbs inference works as expected", {
+    assignments_bern <- clone_id(A, D, C = tree$Z, model = "Bernoulli")
+    expect_is(assignments_bern, "list")
+})
+
+test_that("Bernoulli EM inference works as expected", {
+    assignments_bern_EM <- clone_id(A, D, C = tree$Z, model = "Bernoulli",
+                                    inference = "EM")
+    expect_is(assignments_bern_EM, "list")
+})
+
