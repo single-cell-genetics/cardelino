be49383d23ace44171956b29b1ed3c6b92956761
Yuanhua Huang <huangyh094@gmail.com>
2018-08-18 11:07:44 +0100

    remove get_prob_gap as it is included in get_prob_value

--- R/base_utils.R
+++ R/base_utils.R
@@ -1,22 +1,5 @@
 # A few basic utility functions
 
-#' Get the probability gap between the highest and the second highest assignmnet
-#' probabilities.
-#' 
-#' @export
-#' 
-#' @param prob_assign A matrix of floats. Clonal assignment probability of N 
-#' cells to K clones.
-get_prob_gap <- function(prob_assign){
-    prob_gap <- rep(0, dim(prob_assign)[1])
-    for (j in seq_len(length(prob_gap))){
-        prob_sorted <- sort(prob_assign[j,], decreasing = TRUE)
-        prob_gap[j] <- prob_sorted[1] - prob_sorted[2]
-    }
-    prob_gap
-}
-
-
 #' Get the collapsed probability value
 #' 
 #' the collapsed probability value includes 1) the best probability, 2) the
@@ -30,16 +13,16 @@ get_prob_gap <- function(prob_assign){
 #' best, second and delta. Best: highest probability of a cell to K clones, 
 #' similarly for second. delta is the difference between the best and second.
 #' 
-get_prob_value <- function(prob_assign, mode="delta"){
+get_prob_value <- function(prob_assign, mode="best") {
     prob_val <- rep(0, nrow(prob_assign))
-    for(i in seq_len(length(prob_val))){
-        prob_sorted <- sort(prob_assign[i,], decreasing=T)
-        if(mode == "best"){
-            prob_val[i] <- prob_sorted[1]
-        }else if(mode == "second"){
-            prob_val[i] <- prob_sorted[2]
-        }else{ #default mode: delta
+    for (i in seq_len(length(prob_val))) {
+        prob_sorted <- sort(prob_assign[i,], decreasing = TRUE)
+        if (mode == "delta") {
             prob_val[i] <- prob_sorted[1] - prob_sorted[2]
+        } else if (mode == "second") {
+            prob_val[i] <- prob_sorted[2]
+        } else { #default mode: best
+            prob_val[i] <- prob_sorted[1]
         }
     }
     prob_val
