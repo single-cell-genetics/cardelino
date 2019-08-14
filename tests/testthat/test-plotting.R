270842713e00a5f97450ca5931e9cc0e23df818f
huangyh09 <huangyh094@gmail.com>
2019-08-14 23:45:23 +0100

    v0.6.2: update clone_id convergence diagnostic
    Former-commit-id: ce5f00a1946b08b22ab9426eb52b69aa5257d464

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -5,20 +5,19 @@ context("test plotting")
 
 data(example_donor)
 
-assignments <- clone_id(A_clone, D_clone, Config = tree$Z)
-
-test_that("heatmap for assignment probability works as expected", {
-    fig1 <- prob_heatmap(assignments$prob)
-    expect_is(fig1, "ggplot")
-})
-
-test_that("pheatmap for variants probability across cells works as expected", {
-    fig2 <- vc_heatmap(assignments$prob_variant, assignments$prob, tree$Z)
-    expect_is(fig2, "pheatmap")
-})
-
 test_that("plot tree works as expected", {
     fig3 <- plot_tree(tree, orient = "v")
     expect_is(fig3, "ggtree")
+})
+
+test_that("plot_config_diffs works as expected", {
+    Config1 <- matrix(c(rep(0, 15), rep(1, 8), rep(0, 7), 
+                        rep(1, 5), rep(0, 3), rep(1, 7)), ncol = 3)
+    Config2 <- matrix(c(rep(0, 15), rep(1, 8), rep(1, 7), 
+                        rep(0, 5), rep(1, 3), rep(1, 7)), ncol = 3)
+    rownames(Config1) <- rownames(Config2) <- paste0("var", 1:nrow(Config1))
+    colnames(Config1) <- colnames(Config2) <- paste0("clone", 1:ncol(Config1))
+    fig4 <- plot_config_diffs(Config1, Config2) + pub.theme()
+    expect_is(fig4, "ggplot")
 })
 
