c9274b1e7a559428626572e7889d1cbec9c844e6
Jeffrey Pullin <jeffrey.pullin@gmail.com>
2022-05-20 15:52:37 +1000

    Reduce the number of lines >80 characters
    Former-commit-id: 1a4bd147c295991bcd57b3bfd861a7c2630ff4d0

--- R/binomial_models.R
+++ R/binomial_models.R
@@ -6,17 +6,19 @@
 #' @param k A vector of integers. number of success
 #' @param n A vector of integers. number of trials
 #' @param n_components A number. number of components
-#' @param p_init A vector of floats with length n_components, the initial value of p
+#' @param p_init A vector of floats with length n_components, the initial value
+#'   of p
 #' @param learn_p bool(1) or a vector of bool, wheter learn each p
 #' @param max_iter integer(1). number of maximum iterations
 #' @param min_iter integer(1). number of minimum iterations
 #' @param logLik_threshold A float. The threshold of logLikelihood increase for
 #' detecting convergence
 #'
-#' @return a list containing \code{p}, a vector of floats between 0 and 1 giving
-#' the estimated success probability for each component, \code{psi}, estimated
-#' fraction of each component in the mixture, and \code{prob}, the matrix of
-#' fitted probabilities of each observation belonging to each component.
+#' @return a list containing \code{p}, a vector of floats between 0 and 1
+#' giving the estimated success probability for each component, \code{psi},
+#' estimated fraction of each component in the mixture, and \code{prob}, the
+#' matrix of fitted probabilities of each observation belonging to each
+#' component.
 #'
 #' @import stats
 #'
@@ -100,7 +102,8 @@ mixBinom <- function(k, n, n_components = 2, p_init = NULL, learn_p = TRUE,
 #' RV_pred <- predMixBinom(3, 10, RV$p, RV$psi)
 predMixBinom <- function(k, n, p, psi) {
     if (length(p) != length(psi)) {
-        stop("p and psi must be of the same length (number of mixture components)\n")
+        stop("p and psi must be of the same length ",
+             "(number of mixture components)\n")
     }
     if (length(k) != length(n)) {
         stop("n and k must be of the same length (number of observations)\n")
@@ -109,7 +112,8 @@ predMixBinom <- function(k, n, p, psi) {
     ## assignment probability
     logLik_mat <- matrix(nrow = length(n), ncol = length(p))
     for (j in seq_len(length(p))) {
-        logLik_mat[, j] <- log(psi[j]) + dbinom(k, size = n, prob = p[j], log = TRUE)
+        logLik_mat[, j] <- log(psi[j]) + dbinom(k, size = n, prob = p[j],
+                                                log = TRUE)
     }
     logLik_mat_amplify <- logLik_mat - matrixStats::rowMaxs(logLik_mat)
     prob_mat <- exp(logLik_mat_amplify) / rowSums(exp(logLik_mat_amplify))
